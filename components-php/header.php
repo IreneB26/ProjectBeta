087d9a353eb381ff03f275d6f8a3487a1fabd6fa
IreneB26 <bargues.irene@gmail.com>
2022-03-24 00:57:08 +0100

    menu detail

--- components-php/header.php
+++ components-php/header.php
@@ -38,9 +38,9 @@
     </button>
 
     <ul class="nav-menu">
-      <li class="nav-menu-item"><a href="#1" class="nav-menu-link">ME!</a></li>
-      <li class="nav-menu-item"><a href="#2" class="nav-menu-link">Objetivo</a></li>
-      <li class="nav-menu-item"><a href="#3" class="nav-menu-link">Skills</a></li>
+      <li class="nav-menu-item"><a href="#1" class="nav-menu-link">Crea tu evento</a></li>
+      <li class="nav-menu-item"><a href="#2" class="nav-menu-link">Inicio de sesión</a></li>
+      <li class="nav-menu-item"><a href="#3" class="nav-menu-link">Carrito</a></li>
       <li class="nav-menu-item"><a href="#footer" class="nav-menu-link">Contacto</a></li>
 
 

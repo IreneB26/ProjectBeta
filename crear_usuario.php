0edfcc755a949be7a5a0867fc82315925aa8c1ba
IreneB26 <bargues.irene@gmail.com>
2022-03-24 19:35:20 +0100

    login y creacion de usuario

--- crear_usuario.php
+++ crear_usuario.php
@@ -7,10 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
 
-    <title>ticker 2 you</title>
+    <title>ticket2you</title>
 </head>
 
-<body class="inicio_sesion">
+<body class="crear_usuario">
 
     <video class="video_inicio_sesion" autoplay="autoplay" loop="loop" muted defaultMuted>
         <source src="imagenes/video.mp4" type="video/mp4">
@@ -23,19 +23,22 @@
 
     <form class="formulario_inicio_sesion" method="post" action="principal.php">
 
-        <h1 class="title_footer_form inicio_sesion_title ">Login</h1>
-        <p class="content_footer_form  inicio_sesion_text">Entra y disfruta de todas las opciones de la web</p>
+        <h1 class="title_footer_form inicio_sesion_title ">Registro</h1>
+        <p class="content_footer_form  inicio_sesion_text">Crea tu usuario para acceder a la web</p>
 
         <label class="label label_sesion" for="email">Correo</label>
         <input class="input input_sesion" type="email" id="email" name="email" placeholder="ticket2you@events.com">
         <label class="label label_sesion" for="mensaje">Contraseña</label>
-        <textarea class="input input_sesion" type="pasword" id="contraseña" name="contraseña" placeholder="Password"></textarea>
+        <input class="input input_sesion" type="pasword" id="contraseña" name="contraseña" placeholder="Password">
+        <label class="label label_sesion" for="mensaje">Repetir contraseña</label>
+        <input class="input input_sesion" type="pasword" id="contraseña" name="contraseña" placeholder="Password">
+
         <input class="boton boton_sesion" type="submit" value="Enviar" name="submit">
     </form>
 
     <div class="crear_cuenta">
 
-        <p class="p_registro">¿No estás registrado? <a class="a_registro" href="crear_usuario.php">Crea una cuenta</a></p>
+        <p class="p_registro">¿No estás registrado? <a class="a_registro" href="#">Crea una cuenta</a></p>
 
     </div>
 

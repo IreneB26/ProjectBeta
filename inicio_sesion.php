f8dc7b4305067f85a2f6728969a9c51f219ad206
IreneB26 <bargues.irene@gmail.com>
2022-03-24 01:48:26 +0100

    inicio sesion estructura

---
+++ inicio_sesion.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>ticker 2 you</title>
+</head>
+
+<body class="inicio_sesion">
+
+    <h1 class="logo_incio_sesion">LOGO</h1>
+
+
+    <form class="formulario_inicio_sesion" method="post" action="principal.php">
+
+        <h1 class="title_footer_form inicio_sesion_title ">Login</h1>
+        <p class="content_footer_form  inicio_sesion_text">Entra y disfruta de todas las opciones de la web</p>
+
+        <label class="label label_sesion" for="email">Correo</label>
+        <input class="input input_sesion" type="email" id="email" name="email" placeholder="ticket2you@events.com">
+        <label class="label label_sesion" for="mensaje">Mensaje</label>
+        <textarea class="input input_sesion" type="pasword" id="contraseña" name="contraseña" placeholder="Password"></textarea>
+        <input class="boton boton_sesion" type="submit" value="Enviar" name="submit">
+    </form>
+
+    <div class="crear_cuenta">
+
+        <p>¿No estás registrado? <a href="#">Crea una cuenta</a></p>
+
+    </div>
+
+
+    <!-- <video class="video_inicio_sesion" autoplay="autoplay" loop="loop" muted defaultMuted> 
+        <source src="imagenes/video.mp4" type="video/mp4">
+    </video> -->
+    <!-- 
+    <video class="video_inicio_sesion_mobile" autoplay="autoplay" loop="loop" muted defaultMuted> 
+        <source src="imagenes/inicio_sesion_mobile.mp4" type="video/mp4">
+    </video> -->
+
+</body>
+
+</html>

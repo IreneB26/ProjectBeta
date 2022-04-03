92b24d31ec243585a12cfdf3ad3f6c166c9b5a5a
IreneB26 <bargues.irene@gmail.com>
2022-04-04 01:11:19 +0200

    details styles

--- components-php/busca_evento.php
+++ components-php/busca_evento.php
@@ -4,7 +4,7 @@
     <div class="busqueda">
 
         <input class="form_search" id="txtbusca" type="text" name="busqueda" placeholder="Busca tu evento">
-        <input class="form_submit" type="submit" name="enviar">
+        <a href="#section3" class="form_submit">Enviar</a>
 
     </div>
 

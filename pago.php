6d8e3b7531e62ae833653f1a2d5353e810034e20
IreneB26 <bargues.irene@gmail.com>
2022-04-03 17:45:22 +0200

    styles form

--- pago.php
+++ pago.php
@@ -25,29 +25,36 @@ $precio = $_REQUEST['precio'];
 <div class="container_pago">
 
 
+
     <form class="formulario_pago" method="post" action="">
 
+        <div class="titles_pago">
+            <h1 class="title_h1">Método de pago</h1>
+
+            <p class="sub_title">Introduzca sus detos para ejecutar el pago</p>
+
+        </div>
         <div>
-            <label class="label_pago " for="email">Nombre</label>
-            <input class="input_pago " type="text" name="email" placeholder="Nombre">
+            <label class="label_pago label " for="email">Nombre</label>
+            <input class="input_pago input " type="text" name="email" placeholder="Nombre">
 
         </div>
 
         <div class="email_number">
 
-            <label class="label_pago " for="email">Correo</label>
-            <input class="input_pago " type="email" name="email" placeholder="EventExperience@events.com">
+            <label class="label_pago label " for="email">Correo</label>
+            <input class="input_pago input " type="email" name="email" placeholder="EventExperience@events.com">
 
-            <label class="label_pago " for="email">Teléfono</label>
-            <input class="input_pago " type="email" name="email" placeholder="EventExperience@events.com">
+            <label class="label_pago label" for="email">Teléfono</label>
+            <input class="input_pago input " type="email" name="email" placeholder="EventExperience@events.com">
 
         </div>
 
         <div>
 
 
-            <label class="label_pago " for="mensaje">Tarjeta</label>
-            <input class="input_pago " type="password" name="clave" placeholder="Password">
+            <label class="label_pago label " for="mensaje">Tarjeta</label>
+            <input class="input_pago input" type="password" name="clave" placeholder="Password">
         </div>
         <input class="boton_pago submit_card" type="submit" value="Pagar €<?php echo $precio; ?>" name="submit">
     </form>
